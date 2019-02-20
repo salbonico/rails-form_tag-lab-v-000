c1f541b568e7715554c9876fda242fb1dac05c4e
Steve Albonico <salbonico@gmail.com>
2019-02-20 23:14:57 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -8,4 +8,10 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+  Students.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+  end
 end
