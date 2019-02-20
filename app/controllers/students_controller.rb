8e6f3fa377fa8fdac30976e9ea30cb9ec3293047
Steve Albonico <salbonico@gmail.com>
2019-02-20 23:20:59 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-  Students.create(first_name: params[:first_name], last_name: params[:last_name])
+  Student.create(first_name: params[:first_name], last_name: params[:last_name])
   redirect_to student_path
   end
 end
